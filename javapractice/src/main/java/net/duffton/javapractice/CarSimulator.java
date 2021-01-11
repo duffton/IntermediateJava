b04b1722ebbe8395f99d60d39dca0b2c306956de
duffton <duffton@gmail.com>
2021-01-11 18:24:27 +0000

    Fields, dot notation, and object state.

--- javapractice/src/main/java/net/duffton/javapractice/CarSimulator.java
+++ javapractice/src/main/java/net/duffton/javapractice/CarSimulator.java
@@ -6,6 +6,13 @@ public class CarSimulator {
         System.out.println("Car Simulator running...");
 
         Car myToyota = new Car();
+
+        System.out.println("The car colour is : " + myToyota.colour);
+
+        myToyota.colour = "Blue";
+
+        System.out.println("Got a paint job. New colour is: " + myToyota.colour);
+
     }
 
 }
