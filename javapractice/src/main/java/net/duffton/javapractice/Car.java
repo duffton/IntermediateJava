b9dfcad0dc7d37c9a147010aa89b74b12139916c
duffton <duffton@gmail.com>
2021-01-11 18:17:31 +0000

    Classes and objects demystified.

---
+++ javapractice/src/main/java/net/duffton/javapractice/Car.java
@@ -0,0 +1,14 @@
+package net.duffton.javapractice;
+
+// Car class blueprint for a Car object
+public class Car {
+
+    // Car properties
+    public String colour = "Black";
+
+    // Car constructor
+    public Car() {
+        // additional instructions go here
+    }
+
+}
--- javapractice/src/main/java/net/duffton/javapractice/CarSimulator.java
+++ javapractice/src/main/java/net/duffton/javapractice/CarSimulator.java
@@ -4,6 +4,8 @@ public class CarSimulator {
 
     public static void main(String[] args) {
         System.out.println("Car Simulator running...");
+
+        Car myToyota = new Car();
     }
 
 }
